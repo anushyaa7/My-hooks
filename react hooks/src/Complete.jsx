5ab58ebba2505d7c4e0c62515d6e5060a12779fc
Anushya <anuselva2000@gamil.com>
2024-07-24 18:43:02 +0530

    style changes

--- react hooks/src/Complete.jsx
+++ react hooks/src/Complete.jsx
@@ -3,29 +3,51 @@ import TextField from '@mui/material/TextField';
 import Stack from '@mui/material/Stack';
 import Autocomplete from '@mui/material/Autocomplete';
 import './Complete.css';
+import { createTheme, ThemeProvider, useTheme } from '@mui/material';
+
 
 export default function Complete() {
+  const theme = useTheme(); // Get the current theme
+
   return (
-    <Stack spacing={2} sx={{ width: 300 }} className="autocomplete-container">
+    <div className="autocomplete-container">
       <Autocomplete
         id="free-solo-demo"
         freeSolo
         options={top100Films.map((option) => option.title)}
         renderInput={(params) => 
+          <div style={{ position: 'relative' }}>
             <TextField
-            {...params}
-            label="Placeholder"
-            size="small" // Reduce the size of the TextField
-            margin="dense" // Reduce the margin
-            sx={{ minWidth: '200px' }} // Customize the width if needed
-          />
+              {...params}
+              placeholder="Placeholder" 
+              size="small" 
+              margin="dense" 
+              InputLabelProps={{
+                shrink: false, 
+              }}
+              InputProps={{
+                ...params.InputProps,
+                style: {
+                  padding: '8px 0', 
+                },
+              }}
+              sx={{ 
+                minWidth: '200px',
+                borderRadius: '8px', 
+                '& .MuiOutlinedInput-root': {
+                  borderRadius: '8px', 
+                },
+              }} 
+            />
+           
+          </div>
         }
         classes={{
           paper: 'autocomplete-paper', 
           option: 'autocomplete-option', 
         }}
       />
-    </Stack>
+    </div>
   );
 }
 
